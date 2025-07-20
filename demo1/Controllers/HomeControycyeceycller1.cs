fd954ebf3ed6dfe6de9539725033973617ee2af3
khuongquang124 <2200007143@nttu.edu.vn>
2025-07-20 14:41:55 +0700

    thanhcong

---
+++ demo1/Controllers/HomeControycyeceycller1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demo1.Controllers
+{
+    public class HomeControycyeceycller1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
